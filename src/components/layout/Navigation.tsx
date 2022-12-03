a9752c177b6abd26805bf1ed6a477549045af328
trungluongwww <trungluongwww@gmail.com>
2022-12-03 12:41:10 +0700

    update navigate

--- src/components/layout/Navigation.tsx
+++ src/components/layout/Navigation.tsx
@@ -1,5 +1,5 @@
-import React, { useState, useContext, useEffect } from "react";
-import { NavLink } from "react-router-dom";
+import React, {useState, useContext, useEffect} from "react";
+import {NavLink} from "react-router-dom";
 import useWindowScrollPosition from "@rehooks/window-scroll-position";
 import useMediaQuery from "@material-ui/core/useMediaQuery";
 import CartPreview from "./CartPreview";
@@ -7,100 +7,199 @@ import Search from "./Search";
 import logo from "../../img/Logo/logo.png";
 import AccountCircleIcon from "@mui/icons-material/AccountCircle";
 import LoginIcon from "@mui/icons-material/Login";
-import { AppContext } from "../../context/Context";
+import {AppContext} from "../../context/Context";
 import userAPI from "../../api/userFunction";
+import ReceiptLongIcon from '@mui/icons-material/ReceiptLong';
+import ExitToAppIcon from '@mui/icons-material/ExitToApp';
+import {
+    Avatar,
+    Button,
+    Dialog,
+    DialogTitle,
+    List,
+    ListItem,
+    ListItemAvatar, ListItemText,
+    Menu,
+    MenuItem,
+    Select
+} from "@mui/material";
+import Typography from "@mui/material/Typography";
+import AddIcon from '@mui/icons-material/Add';
+
+export interface SimpleDialogProps {
+    open: boolean;
+    selectedValue: string;
+    onClose: (value: string) => void;
+}
+
+function SimpleDialog(props: SimpleDialogProps) {
+    const {onClose, selectedValue, open} = props;
+
+    const handleClose = () => {
+        onClose(selectedValue);
+    };
+
+    const handleListItemClick = (value: string) => {
+        onClose(value);
+    };
+
+    return (
+        <Dialog onClose={handleClose} open={open}>
+            <List sx={{pt: 0}}>
+                <ListItem onClick={() => handleListItemClick('addAccount')}>
+                    <NavLink to="/m/profile" style={{display: "flex", justifyContent: "center", alignItems: "center"}}>
+                        <ListItemAvatar>
+                            <Avatar>
+                                <AccountCircleIcon/>
+                            </Avatar>
+                        </ListItemAvatar>
+                        <ListItemText primary="Thông tin cá nhân"/>
+                    </NavLink>
+                </ListItem>
+                <ListItem onClick={() => handleListItemClick('addAccount')}>
+                    <NavLink to="/m/order" style={{display: "flex", justifyContent: "center", alignItems: "center"}}>
+                        <ListItemAvatar>
+                            <Avatar>
+                                <ReceiptLongIcon/>
+                            </Avatar>
+                        </ListItemAvatar>
+                        <ListItemText primary="Lịch sử đơn hàng"/>
+                    </NavLink>
+                </ListItem>
+                <ListItem onClick={() => handleListItemClick('addAccount')}>
+                    <NavLink onClick={(e)=>{
+                        localStorage.removeItem('jwtToken')
+                        localStorage.removeItem('role')
+                    }} to="/sign-in" style={{display: "flex", justifyContent: "center", alignItems: "center"}}>
+                        <ListItemAvatar>
+                            <Avatar>
+                                <ExitToAppIcon/>
+                            </Avatar>
+                        </ListItemAvatar>
+                        <ListItemText primary="Đăng xuất"/>
+                    </NavLink>
+                </ListItem>
+            </List>
+        </Dialog>
+    );
+}
+
 
 export default function Navigation() {
-  const { auth, setAuth } = useContext(AppContext);
+    const {auth, setAuth} = useContext(AppContext);
 
-  useEffect(() => {
-    async function fetchData() {
-      const information = await userAPI.getInforUser();
-      setAuth(information);
+    //
+    const [open, setOpen] = React.useState(false);
+    const [selectedValue, setSelectedValue] = React.useState('');
+
+    const handleClickOpen = () => {
+        setOpen(true);
+    };
+
+    const handleClose = (value: string) => {
+        setOpen(false);
+        setSelectedValue(value);
+    };
+
+    //
+
+    useEffect(() => {
+        async function fetchData() {
+            const information = await userAPI.getInforUser();
+            setAuth(information);
+        }
+
+        if (auth !== null) {
+            fetchData();
+        }
+    }, []);
+
+    const [change, setChange] = useState(false);
+    const changePosition = 50;
+    let position = useWindowScrollPosition();
+    if (position.y > changePosition && !change) {
+        setChange(true);
     }
-    if (auth !== null) {
-      fetchData();
+    if (position.y <= changePosition && change) {
+        setChange(false);
     }
-  }, []);
+    let style = {
+        backgroundColor: change ? "rgb(243, 243, 243)" : "transparent",
+        transition: "400ms ease",
+    };
+    let logoStyle = {
+        width: change ? "50px" : "60px",
+        transition: "400ms ease",
+        borderRadius: "50%",
+        padding: "5px",
+    };
+    let navlinkLogoStyle = {
+        display: "flex",
+        alignItems: "center",
+    };
+    let navlinkStyle = {
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+    };
 
-  const [change, setChange] = useState(false);
-  const changePosition = 50;
-  let position = useWindowScrollPosition();
-  if (position.y > changePosition && !change) {
-    setChange(true);
-  }
-  if (position.y <= changePosition && change) {
-    setChange(false);
-  }
-  let style = {
-    backgroundColor: change ? "rgb(243, 243, 243)" : "transparent",
-    transition: "400ms ease",
-  };
-  let logoStyle = {
-    width: change ? "50px" : "60px",
-    transition: "400ms ease",
-    borderRadius: "50%",
-    padding: "5px",
-  };
-  let navlinkLogoStyle = {
-    display: "flex",
-    alignItems: "center",
-  };
-  let navlinkStyle = {
-    display: "flex",
-    alignItems: "center",
-    justifyContent: "center",
-  };
+    const isMobile = useMediaQuery("(max-width:599px)");
 
-  const isMobile = useMediaQuery("(max-width:599px)");
+    if (isMobile) {
+        return (
+            <div>
+                <div className="navbar" style={style}>
+                    <div className="logo_mobile">
+                        <NavLink to="/" style={navlinkLogoStyle}>
+                            <img src={logo} alt="logo" style={logoStyle}/>
+                        </NavLink>
+                    </div>
 
-  if (isMobile) {
-    return (
-      <div>
-        <div className="navbar" style={style}>
-          <div className="logo_mobile">
-            <NavLink to="/" style={navlinkLogoStyle}>
-              <img src={logo} alt="logo" style={logoStyle} />
-            </NavLink>
-          </div>
-
-          {auth ? (
-            <>
-              <AccountCircleIcon /> <span>{auth.username}</span>
-            </>
-          ) : (
-            <NavLink to="/sign-in">
-              <LoginIcon />
-            </NavLink>
-          )}
-          <Search />
-          <CartPreview />
-        </div>
-      </div>
-    );
-  } else {
-    return (
-      <div className="navbar" style={style}>
-        <div className="logo_bigger-screen">
-          <NavLink to="/m" style={navlinkLogoStyle}>
-            <img src={logo} alt="logo" style={logoStyle} />
-            ShopBee
-          </NavLink>
-        </div>
-        {auth ? (
-          <>
-            <NavLink to="/m/profile" style={navlinkStyle}>
-              <AccountCircleIcon /> <span>{auth.profile.name}</span>
-            </NavLink>
-          </>
-        ) : (
-          <NavLink to="/sign-in">
-            <LoginIcon />
-          </NavLink>
-        )}
-        <Search />
-        <CartPreview />
-      </div>
-    );
-  }
+                    {auth ? (
+                        <>
+                            <AccountCircleIcon/> <span>{auth.username}</span>
+                        </>
+                    ) : (
+                        <NavLink to="/sign-in">
+                            <LoginIcon/>
+                        </NavLink>
+                    )}
+                    <Search/>
+                    <CartPreview/>
+                </div>
+            </div>
+        );
+    } else {
+        return (
+            <div className="navbar" style={style}>
+                <div className="logo_bigger-screen">
+                    <NavLink to="/m" style={navlinkLogoStyle}>
+                        <img src={logo} alt="logo" style={logoStyle}/>
+                        ShopBee
+                    </NavLink>
+                </div>
+                {auth ? (
+                    <>
+                        <div>
+                            <br/>
+                            <Button variant="outlined" onClick={handleClickOpen}>
+                                {auth.name}
+                            </Button>
+                            <SimpleDialog
+                                selectedValue={selectedValue}
+                                open={open}
+                                onClose={handleClose}
+                            />
+                        </div>
+                    </>
+                ) : (
+                    <NavLink to="/sign-in">
+                      <LoginIcon />
+                    </NavLink>
+                )}
+                <Search/>
+                <CartPreview/>
+            </div>
+        );
+    }
 }
